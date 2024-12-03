bbe1e50a4cd1f238e7df89c82781746f149e3d59
whatmakeart <whatmakeart@gmail.com>
2024-12-03 16:44:59 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,14 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
